5990c9b66691ebd9df16b52f166a4863dd933cba
windsmith <shriramsekar11@gmail.com>
2024-07-20 14:44:55 +0400

    feat: add lives on UI

--- Scripts/teacher_view.gd
+++ Scripts/teacher_view.gd
@@ -8,6 +8,8 @@ var count = 0;
 @onready var cooldown = get_node("Cooldown")
 @export var cooldownDuration = 3
 
+@onready var lives = $"../../../../CanvasLayer/Control"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var points = PackedVector2Array([Vector2(0, height), Vector2(-base / 2, 0), Vector2(base / 2, 0)])
@@ -20,6 +22,7 @@ func _process(delta):
 	if self.is_colliding(): 
 		if cooldown.is_stopped():
 			if self.get_collider(0).get_parent().isCheating:
+				lives.get_children()[count].play("on")
 				count+=1
 				if count == 3: 
 					get_tree().change_scene_to_file("res://Scenes/lose_screen.tscn")
